c9603ecdd67dbf96987fb8a6585d83ff93626268
NelaRepko <repkonela@gmail.com>
2026-04-01 21:54:29 +0300

    add project name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/03-react-movies/',  // <- додали сюди назву репозиторію
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
